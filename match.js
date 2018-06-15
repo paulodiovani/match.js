d3fea5e11ae4e30f675c3237760431870c27cd7a
Paulo Diovani <paulo@diovani.com>
2018-06-15 18:43:29 -0300

    Add first trial implementation

--- match.js
+++ match.js
@@ -1,5 +1,28 @@
-const match = () => {
+const emptyFn = () => {}
 
+const functionList = []
+
+const _ = () => {}
+
+const draw = (name) => (...args) => {
+  const selected = functionList.find((item) => {
+    if (item.name !== name) return false
+    if (item.pattern.length === 0) return true
+    if (item.pattern.some((m, i) => m === args[i])) return true
+    return false
+  })
+
+  return selected.fn(...args)
 }
 
-module.exports = match
+const def = (name, pattern = [], fn = emptyFn) => {
+  functionList.push({
+    name,
+    pattern,
+    fn
+  })
+
+  return draw(name)
+}
+
+module.exports = { _, def, draw }
--- match_spec.js
+++ match_spec.js
@@ -1,11 +1,44 @@
 const { expect, use } = require('chai')
 const dirtyChai = require('dirty-chai')
-const def = require('./match')
+const { _, def, draw } = require('./match')
 
 use(dirtyChai)
 
 describe('Match.js', () => {
-  it('returns a function', () => {
-    expect(def).to.be.a('function')
+  describe('#def', () => {
+    it('is a function', () => {
+      expect(def).to.be.a('function')
+    })
+
+    it('returns a function', () => {
+      expect(def('ignoreMe')).to.be.a('function')
+    })
+  })
+
+  describe('#draw', () => {
+    it('is a function', () => {
+      expect(draw).to.be.a('function')
+    })
+
+    it('returns a function', () => {
+      expect(draw('ignoreMe')).to.be.a('function')
+    })
+
+    context('when there are defined functions', () => {
+      before(() => {
+        def('foobar', ['foo', _], (a, b) => `a is foo, b is ${b}`)
+        def('foobar', ['bar', _], (a, b) => `a is bar, b is ${b}`)
+      })
+
+      it('matches when left arg is foo', () => {
+        const foobar = draw('foobar')
+        expect(foobar('foo', 99)).to.equal('a is foo, b is 99')
+      })
+
+      it('matches when left arg is bar', () => {
+        const foobar = draw('foobar')
+        expect(foobar('bar', 2000)).to.equal('a is bar, b is 2000')
+      })
+    })
   })
 })
